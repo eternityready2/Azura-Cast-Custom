63899e4ffa5bd6b765e4e9c6c8f4e27d295510df
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-08-22 12:24:37 -0500

    Add RadioReg cover art setting

--- frontend/components/Stations/Webhooks/Form/RadioReg.vue
+++ frontend/components/Stations/Webhooks/Form/RadioReg.vue
@@ -19,37 +19,49 @@
                 :label="$gettext('RadioReg Organization API Key')"
                 :description="$gettext('An API token is issued on a per-organization basis and are found on the org. settings page.')"
             />
+
+            <form-group-checkbox
+                id="form_config_send_cover_art"
+                class="col-md-12"
+                :field="r$.config.send_cover_art"
+                :label="$gettext('Send Cover Art')"
+                :description="$gettext('If enabled, the cover art URLs for the current and next song will be included in the webhook payload. If disabled, RadioReg will look up cover art on its own.')"
+            />
         </div>
     </tab>
 </template>
 
 <script setup lang="ts">
-import FormGroupField from "~/components/Form/FormGroupField.vue";
+import { required } from "@regle/rules";
+import { storeToRefs } from "pinia";
+import { Ref } from "vue";
 import Tab from "~/components/Common/Tab.vue";
-import {WebhookComponentProps} from "~/components/Stations/Webhooks/EditModal.vue";
-import {useAppScopedRegle} from "~/vendor/regle.ts";
-import {required} from "@regle/rules";
-import {useFormTabClass} from "~/functions/useFormTabClass.ts";
-import {storeToRefs} from "pinia";
-import {useStationsWebhooksForm} from "~/components/Stations/Webhooks/Form/form.ts";
-import {Ref} from "vue";
-import {WebhookRecordCommon, WebhookRecordRadioReg} from "~/entities/Webhooks.ts";
+import FormGroupCheckbox from "~/components/Form/FormGroupCheckbox.vue";
+import FormGroupField from "~/components/Form/FormGroupField.vue";
+import { WebhookComponentProps } from "~/components/Stations/Webhooks/EditModal.vue";
+import { useStationsWebhooksForm } from "~/components/Stations/Webhooks/Form/form.ts";
+import {
+    WebhookRecordCommon,
+    WebhookRecordRadioReg,
+} from "~/entities/Webhooks.ts";
+import { useFormTabClass } from "~/functions/useFormTabClass.ts";
+import { useAppScopedRegle } from "~/vendor/regle.ts";
 
 defineProps<WebhookComponentProps>();
 
-const {form} = storeToRefs(useStationsWebhooksForm());
+const { form } = storeToRefs(useStationsWebhooksForm());
 
-const {r$} = useAppScopedRegle(
+const { r$ } = useAppScopedRegle(
     form as Ref<WebhookRecordCommon & WebhookRecordRadioReg>,
     {
         config: {
-            webhookurl: {required},
-            apikey: {required}
-        }
+            webhookurl: { required },
+            apikey: { required },
+        },
     },
     {
-        namespace: 'station-webhooks'
-    }
+        namespace: "station-webhooks",
+    },
 );
 
 const tabClass = useFormTabClass(r$);
