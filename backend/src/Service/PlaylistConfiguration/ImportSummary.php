76a06af8f5d0e9311c590aeeb6ae7acf25dbb3ac
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-08-22 11:46:48 -0500

    Add playlist import result summary

---
+++ backend/src/Service/PlaylistConfiguration/ImportSummary.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\PlaylistConfiguration;
+
+use App\Entity\StationMedia;
+use App\Entity\StationPlaylist;
+
+final class ImportSummary
+{
+    /** @var array<string, StationPlaylist> */
+    public array $playlistsByRef = [];
+
+    /** @var array<string, StationMedia> */
+    public array $mediaByRef = [];
+
+    public int $playlistsCreated {
+        get => count($this->playlistsByRef);
+    }
+
+    public int $foldersCreated = 0;
+    public int $schedulesCreated = 0;
+    public int $mediaItemsCreated = 0;
+    public int $mediaRelinked = 0;
+    public int $mediaGenerated = 0;
+    public int $membersCreated = 0;
+    public int $smartBlockCriteriaCreated = 0;
+
+    /** @var string[] */
+    public array $warnings = [];
+}
