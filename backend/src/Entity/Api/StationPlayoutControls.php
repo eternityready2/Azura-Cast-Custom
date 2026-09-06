cbf0971fced52774a386d6af258d274ac430289b
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-09-06 17:13:14 -0500

    Use generic typed playout configuration

--- backend/src/Entity/Api/StationPlayoutControls.php
+++ backend/src/Entity/Api/StationPlayoutControls.php
@@ -14,10 +14,6 @@ use OpenApi\Attributes as OA;
 )]
 final readonly class StationPlayoutControls
 {
-    private const bool DEFAULT_STRETCH_SQUEEZE_ENABLED = true;
-
-    private const float DEFAULT_STRETCH_SQUEEZE_MAX_PERCENT = 5.0;
-
     #[OA\Property]
     public bool $hard_clock_enabled;
 
@@ -44,19 +40,13 @@ final readonly class StationPlayoutControls
 
     public function __construct(StationBackendConfiguration $config)
     {
-        $rawConfig = $config->toArray(true) ?? [];
-
-        $this->hard_clock_enabled = $config->top_of_hour_hard_trigger_enabled;
-        $this->hard_clock_trigger_seconds = $config->top_of_hour_hard_trigger_seconds;
-        $this->hard_clock_fade_seconds = $config->top_of_hour_hard_trigger_fade;
-        $this->stretch_squeeze_enabled = (bool)(
-            $rawConfig['playout_stretch_squeeze_enabled'] ?? self::DEFAULT_STRETCH_SQUEEZE_ENABLED
-        );
-        $this->stretch_squeeze_max_percent = (float)(
-            $rawConfig['playout_stretch_squeeze_max_percent'] ?? self::DEFAULT_STRETCH_SQUEEZE_MAX_PERCENT
-        );
-        $this->smart_duck_enabled = $config->top_of_hour_duck_enabled;
-        $this->smart_duck_attenuation = $config->top_of_hour_duck_attenuation;
-        $this->smart_duck_delay = $config->top_of_hour_duck_delay;
+        $this->hard_clock_enabled = $config->playout_hard_clock_enabled;
+        $this->hard_clock_trigger_seconds = $config->playout_hard_clock_trigger_seconds;
+        $this->hard_clock_fade_seconds = $config->playout_hard_clock_fade_seconds;
+        $this->stretch_squeeze_enabled = $config->playout_stretch_squeeze_enabled;
+        $this->stretch_squeeze_max_percent = $config->playout_stretch_squeeze_max_percent;
+        $this->smart_duck_enabled = $config->playout_smart_duck_enabled;
+        $this->smart_duck_attenuation = $config->playout_smart_duck_attenuation;
+        $this->smart_duck_delay = $config->playout_smart_duck_delay;
     }
 }
